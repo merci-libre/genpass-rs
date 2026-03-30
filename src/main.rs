18ce428357b323a6a693b259f81aedaeb1a9a294
withoutdishonor <westwardfishdme@gmail.com>
2026-03-30 04:41:20 -0500

    total refactor: improved readability of main

--- src/main.rs
+++ src/main.rs
@@ -1,297 +1,192 @@
 // modules
 mod args;
-mod mime;
-mod steganographic;
-mod stringgeneration;
-mod zxcvbn;
 
-use std::{path::Path, process::exit};
+use std::{error::Error, path::Path, process::exit};
 
 // calls to modules
-use args::*; // we can safely glob this
+mod modules;
+use args::{CheckArgs, Commands, GenpassArgs, ImageCommands};
 use clap::Parser;
+use modules::{passwords, steganographic};
 use std::env;
+
+use crate::args::Password;
 
 const BIN_NAME: &str = env!("CARGO_PKG_NAME");
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 const AUTHOR: &str = env!("CARGO_PKG_AUTHORS");
-/*
- To make things a bit more readable, I rewrote most of my comments inside of the program,
- because I completely forgot what most things did in here. I realized that most of my comments
- that I had originally made were kinda sh*t so I'm going to best explain what my thought
- process was when creating this program.
 
- The main file just does all the argument and error handling for the majority of the program.
- It comprises of 2 primary functions:
+fn enumerate_image_subs(subcommands: ImageCommands, debug: bool) -> Result<(), Box<dyn Error>> {
+    match subcommands {
+        ImageCommands::Generate(new_steganographic_image_options) => {
+            let arguments = new_steganographic_image_options
+                .to_owned()
+                .check_arguments();
+            let result_string = passwords::generator::generate(
+                arguments,
+                new_steganographic_image_options.length,
+                debug,
+            )
+            .get_password()
+            .to_owned();
 
- - main() // matches command line arguments to their specified modules. Provides both parameters,
-             among other things.
+            if !Path::new(&new_steganographic_image_options.name).exists() {
+                eprintln!("file does not exist");
+                exit(1)
+            }
+            let filepath = String::from(&new_steganographic_image_options.name);
+            // check the file's magic bytes.
+            steganographic::mime::check_magic(&filepath)?;
+            {
+                steganographic::image::store(
+                    new_steganographic_image_options.name,
+                    new_steganographic_image_options.output,
+                    &result_string,
+                    new_steganographic_image_options.unencrypted,
+                )?;
+            }
+            println!("{result_string}");
+        }
 
+        ImageCommands::Read(steganographic_read_options) => {
+            let filepath = String::from(&steganographic_read_options.name);
+            // check the file's magic bytes.
+            steganographic::mime::check_magic(&filepath)?;
 
- There will be comments mostly explaining how or why things are set not only in this file, but in
- stringgeneration.rs, db.rs, and whichever modules that may or may not come up.
+            if !Path::new(&filepath).exists() {
+                eprintln!("file does not exist");
+                exit(1)
+            }
+            // First tries to extract an encrypted password-- otherwise fails.
+            match steganographic::image::extract_raw_unencrypted(&steganographic_read_options.name)
+            {
+                Ok(password) => println!("{password}"),
+                Err(_) => {
+                    modules::steganographic::image::extract(&steganographic_read_options.name)
+                }
+            }
+        }
 
- Additionally, I will be creating better documentation on my website, permitted that I have the
- time to do so.
+        ImageCommands::Embed(existing_args) => {
+            // this function is broken right now-- planned revision coming soon.
+            let filepath = String::from(&existing_args.name);
+            if Path::new(&filepath).exists() {
+                let filepath = String::from(&existing_args.name);
+                steganographic::mime::check_magic(&filepath)?;
 
-*/
+                match steganographic::image::store(
+                    existing_args.name,
+                    existing_args.output,
+                    &existing_args.payload.to_string(),
+                    existing_args.unencrypted,
+                    // See documentation for how this function works.
+                ) {
+                    Ok(_) => (),
+                    Err(e) => eprintln!("{} experienced an error: {e}", BIN_NAME),
+                }
+            }
+        }
+    }
+    Ok(())
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // parse the arguments for clap
     let args = GenpassArgs::parse();
     eprintln!("{} v.{} \n- Developed by: {}\n", BIN_NAME, VERSION, AUTHOR);
 
-    let mut result_string: String = String::new();
     let debug = args.debug;
     let command = args.commands;
-    let lp = args.r#loop;
-    /*
-     *
-     * */
-    let min: u8;
-    let mut max: u8 = 0;
+    let pass_loop = args.r#loop;
+
+    let mut arguments: Password = Password::new();
+    let mut length: u8 = 0;
+
+    // options for set alphbetic
+    let mut uppercase = false;
+    let mut lowercase = false;
 
     // argument match cases, need to refactor to reinforce DRY principles?
     match command {
         /* String Command */
-        Commands::String(StringArgs) => {
-            // main stuff
-            let space = StringArgs.space;
-            match space {
-                true => min = 32,
-                false => min = 33,
-            }
-            match StringArgs.encoding.as_str() {
-                "ext" | "extasc" => max = 255,
-                "asc" | "ascii" => max = 127,
-                _ => {
-                    eprintln!("Unknown encoding argument, setting to ASCII");
-                    max = 127
-                }
-            }
-            result_string =
-                stringgeneration::generator(StringArgs.length, min, max, result_string, debug);
-
-            if debug {
-                dbg!(min, max, StringArgs.length);
-                println!("[String Vector]:\n{:#x?}", result_string.as_bytes());
-                println!("Vector Size: {}", result_string.as_bytes().len());
-            }
-
-            // loops the program inside of here. can be refactored into a function (TODO)
-            match lp {
-                true => loop {
-                    result_string = String::new();
-                    result_string = stringgeneration::generator(
-                        StringArgs.length,
-                        min,
-                        max,
-                        result_string,
-                        debug,
-                    );
-                    if debug {
-                        dbg!(min, max, StringArgs.length);
-                        println!("[String Vector]:\n{:#x?}", result_string.as_bytes());
-                        println!("Vector Size: {}", result_string.as_bytes().len());
-                    }
-                    println!("\n{}", result_string);
-                },
-                false => (),
-            }
+        Commands::String(regular_password_options) => {
+            arguments = regular_password_options.to_owned().check_arguments();
+            length = regular_password_options.length;
         }
+
         /* Integer Command */
-        Commands::Integer(IntegerArgs) => {
-            result_string = stringgeneration::intgen(IntegerArgs.length, result_string);
+        Commands::Integer(integer_password) => {
+            arguments = integer_password.to_owned().check_arguments();
+            length = integer_password.length;
 
-            if debug {
-                dbg!(IntegerArgs.length);
-            }
+            if pass_loop {
+                loop {
+                    let password_info = passwords::generator::generate(arguments, length, debug);
+                    let password = password_info.get_password().to_owned();
 
-            match lp {
-                true => loop {
-                    result_string = String::new();
-                    result_string = stringgeneration::intgen(IntegerArgs.length, result_string);
-                    if debug {
-                        dbg!(IntegerArgs.length);
-                        println!("[String Vector]:\n{:#x?}", result_string.as_bytes());
-                    }
-                    println!("\n{}", result_string);
-                },
-                false => (),
+                    println!("\n{}", password);
+                }
             }
         }
         /* Alphanumeric Command */
-        Commands::Alphanumeric(AlphaArgs) => {
-            /*
-             * By default, the starting character will be '0' if no and encompass all letters from
-             * 'A-Z, a-z'. However, if the user specifies that they want only a select case
-             * (i.e, uppercase or lowercase generation ONLY), the program will only generate the
-             * utf-8 codes 'A-Z'. This was done to reduce the frequency of the same character being
-             * generated.
-             */
-
-            let mut min: u8 = 48;
-            let mut max: u8 = 123;
-
-            if AlphaArgs.alphabet {
-                min = 65;
-            }
-            if AlphaArgs.smallcase || AlphaArgs.upper {
-                max = 90;
-            }
+        Commands::Alphanumeric(alphabetic_password) => {
             /*
               We store the result string first in the case that the user only had specified
               uppercase or lowercase letter generation, otherwise the program just finishes.
             */
-            result_string =
-                stringgeneration::alphanumeric(AlphaArgs.length, min, max, result_string);
+            arguments = alphabetic_password.to_owned().check_arguments();
+            length = alphabetic_password.length;
+
             // manage letter cases.
-            if AlphaArgs.upper && !AlphaArgs.smallcase {
-                result_string = result_string.to_uppercase();
+            if alphabetic_password.upper && !alphabetic_password.smallcase {
+                uppercase = true;
             }
-            if AlphaArgs.smallcase && !AlphaArgs.upper {
-                result_string = result_string.to_lowercase();
+            if alphabetic_password.smallcase && !alphabetic_password.upper {
+                lowercase = true;
             }
-            match lp {
-                true => loop {
-                    result_string = String::new();
-                    result_string =
-                        stringgeneration::alphanumeric(AlphaArgs.length, min, max, result_string);
-                    if debug {
-                        dbg!(min, max, AlphaArgs.length);
-                        println!("[String Vector]:\n{:#x?}", result_string.as_bytes());
+
+            if pass_loop {
+                loop {
+                    let password_info = passwords::generator::generate(arguments, length, debug);
+                    let mut password = password_info.get_password().to_owned();
+                    if alphabetic_password.upper && !alphabetic_password.smallcase {
+                        password = password.to_uppercase();
                     }
-                    if AlphaArgs.upper && !AlphaArgs.smallcase {
-                        result_string = result_string.to_uppercase();
+                    if alphabetic_password.smallcase && !alphabetic_password.upper {
+                        password = password.to_lowercase();
                     }
-                    if AlphaArgs.smallcase && !AlphaArgs.upper {
-                        result_string = result_string.to_lowercase();
-                    }
-                    println!("\n{}", result_string);
-                },
-                false => (),
-            }
-            if debug {
-                dbg!(min, max, AlphaArgs.length);
+                    println!("\n{}", password);
+                }
             }
         }
 
         /* Estimate Command */
-        Commands::Estimate(EstimateArgs) => {
-            if EstimateArgs.string.is_empty() {
+        Commands::Estimate(estimate_args) => {
+            if estimate_args.string.is_empty() {
                 eprintln!("cannot parse an empty string");
                 exit(1)
             }
-
-            let score = zxcvbn::estimate(EstimateArgs.string.to_string());
-            print!(
-                "zxcvbn score for '{}': {}\n",
-                EstimateArgs.string.to_string(),
-                score
-            );
-            match score.into() {
-                0 => println!(
-                    "This password is extremely weak, as it would take at least 100 guesses to crack."
-                ),
-                1 => println!(
-                    "This password is moderately weak, as it would take at least 100,000 guesses to crack."
-                ),
-                2 => println!(
-                    "This password is slightly strong, as it would take at least 100,000,000 guesses to crack."
-                ),
-                3 => println!(
-                    "This password is strong, as it would take at least 10,000,000,000 guesses to crack."
-                ),
-                4 => println!(
-                    "This password is extremely strong, as it would take more than 10,000,000,000 guesses to crack."
-                ),
-                _ =>todo!()
-            }
+            let password = estimate_args.string.to_string();
+            modules::passwords::zxcvbn::estimate(password);
         }
 
         /* Steganographic Commands */
-        Commands::Steg(StoreArgs) => {
-            let subcommand = StoreArgs.store;
-            match subcommand {
-                // NewArgs is the command parser for Generate, originally named New, so don't fret
-                // about the ARGs parser being named differently than the actual command name.
-                ImageCommands::Generate(NewArgs) => {
-                    let space = NewArgs.space;
-                    let min = match space {
-                        true => 32,
-                        false => 33,
-                    };
-                    let max = match NewArgs.encoding.as_str() {
-                        "ext" | "extasc" => 255,
-                        "asc" | "ascii" => 127,
-                        _ => {
-                            eprintln!("Unknown encoding argument, setting to ASCII");
-                            127
-                        }
-                    };
-                    if max == 255 && NewArgs.length > 120 {
-                        // error 6: max length for extended ascii/utf-8 cannot exceed 120
-                        // characters
-                        eprintln! ("Error: Using the `extasc` command with `generate` generate strings longer than 120 characters.\n
-                                    This is due to certain technological limitations with the steganography crate.");
-                        exit(1)
-                    }
-                    result_string =
-                        stringgeneration::generator(NewArgs.length, min, max, result_string, debug);
-                    // switch to magic bytes
-                    if Path::new(&NewArgs.name).exists() {
-                        let filepath = String::from(&NewArgs.name);
-                        mime::check_magic(filepath)?;
-                        {
-                            steganographic::store(
-                                NewArgs.name,
-                                NewArgs.output,
-                                result_string.clone(),
-                                NewArgs.unencrypted,
-                            )?;
-                        }
-                    } else {
-                        eprintln!("file does not exist");
-                        exit(1)
-                    }
-                }
-                ImageCommands::Read(ReadArgs) => {
-                    let filepath = String::from(&ReadArgs.name);
-                    mime::check_magic(filepath)?;
-                    match Path::new(&ReadArgs.name).exists() {
-                        //switch to magic bytes by v2
-                        true => match steganographic::extract_raw_unencrypted(&ReadArgs.name) {
-                            Ok(password) => println!("{password}"),
-                            Err(e) => {
-                                eprintln!("{e}");
-                                steganographic::extract(&ReadArgs.name)
-                            }
-                        },
-
-                        false => {
-                            eprintln!("file does not exist");
-                            exit(1)
-                        }
-                    }
-                }
-                ImageCommands::Embed(ExistingArgs) => {
-                    if Path::new(&ExistingArgs.name).exists() {
-                        let filepath = String::from(&ExistingArgs.name);
-                        mime::check_magic(filepath)?;
-                        match steganographic::store(
-                            ExistingArgs.name,
-                            ExistingArgs.output,
-                            ExistingArgs.payload.to_string(),
-                            ExistingArgs.unencrypted,
-                            // See documentation for how this function works.
-                        ) {
-                            Ok(_) => (),
-                            Err(e) => eprintln!("{} experienced an error: {e}", BIN_NAME),
-                        }
-                    }
-                }
-            }
+        Commands::Steg(steganographic_arguments) => {
+            let subcommand = steganographic_arguments.store;
+            enumerate_image_subs(subcommand, debug)?;
+            exit(0);
         }
     }
-    Ok(print!("{result_string}\n"))
+
+    // actually print the string
+    let mut result_string = passwords::generator::generate(arguments, length, debug)
+        .get_password()
+        .to_owned();
+    if uppercase {
+        result_string = result_string.to_lowercase();
+    }
+    if lowercase {
+        result_string = result_string.to_lowercase();
+    }
+    Ok(println!("{result_string}\n"))
 }
