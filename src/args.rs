9d16ce22a700a2d9ccb3544d955dab2bcf77245e
withoutdishonor <westwardfishdme@gmail.com>
2026-03-30 04:42:27 -0500

    total refactor: improved argument parsing

--- src/args.rs
+++ src/args.rs
@@ -98,6 +98,7 @@ pub enum ImageCommands {
     Embed(ExistingArgs),
 }
 
+// Image Structs
 #[derive(Clone, Debug, Args)]
 pub struct NewArgs {
     /// encoding for the characters used in the password. Valid arguments include: 'extasc, ascii'
@@ -139,4 +140,159 @@ pub struct ExistingArgs {
 pub struct ReadArgs {
     /// Name of the image file to read and decrypt.
     pub name: String,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum PasswordType {
+    Alphanumeric,
+    Numeric,
+    Regular,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Password {
+    minimum_character: u8,
+    maximum_character: u8,
+    passtype: PasswordType,
+}
+
+impl Password {
+    pub fn new() -> Password {
+        //! creates a new password.
+        Password {
+            minimum_character: 0,
+            maximum_character: 0,
+            passtype: PasswordType::Regular,
+        }
+    }
+    fn set_min(mut self, minimum: u8) -> Password {
+        self.minimum_character = minimum;
+        return self;
+    }
+    fn set_max(mut self, max: u8) -> Password {
+        self.maximum_character = max;
+        return self;
+    }
+    fn set_passwordtype(mut self, password_type: PasswordType) -> Password {
+        self.passtype = password_type;
+        return self;
+    }
+
+    pub fn password_type(self) -> PasswordType {
+        self.passtype
+    }
+    pub fn min(self) -> u8 {
+        self.minimum_character
+    }
+    pub fn max(self) -> u8 {
+        self.maximum_character
+    }
+}
+
+pub trait CheckArgs {
+    fn check_arguments(self) -> Password;
+    fn get_length(&self) -> u8;
+}
+
+impl CheckArgs for AlphaArgs {
+    fn check_arguments(self) -> Password {
+        let mut min: u8 = 48;
+        let mut max: u8 = 122;
+
+        if self.alphabet {
+            min = 65;
+        }
+        if self.smallcase || self.upper {
+            max = 90;
+        }
+
+        let mut password_struct = Password::new();
+        password_struct = password_struct.set_min(min);
+        password_struct = password_struct.set_max(max);
+        password_struct = password_struct.set_passwordtype(PasswordType::Alphanumeric);
+
+        return password_struct;
+    }
+    fn get_length(&self) -> u8 {
+        return self.length.to_owned();
+    }
+}
+
+impl CheckArgs for IntegerArgs {
+    fn check_arguments(self) -> Password {
+        //! Checks to see if the user has asked for spaces to be generated
+        //! in their password-- if so, then it returns
+
+        let mut password_struct = Password::new();
+        let zero = 48;
+        let nine = 57;
+
+        password_struct = password_struct.set_min(zero);
+        password_struct = password_struct.set_max(nine);
+        password_struct = password_struct.set_passwordtype(PasswordType::Numeric);
+
+        return password_struct;
+    }
+    fn get_length(&self) -> u8 {
+        return self.length.to_owned();
+    }
+}
+
+impl CheckArgs for StringArgs {
+    fn check_arguments(self) -> Password {
+        //! Checks to see if the user has asked for spaces to be generated
+        //! in their password-- if so, then it returns
+
+        let mut password_struct = Password::new();
+        let space = 32;
+        let exclamation_point = 33;
+
+        password_struct = match self.space {
+            true => password_struct.set_min(space),
+            false => password_struct.set_min(exclamation_point),
+        };
+
+        password_struct = match self.encoding.as_str() {
+            "ext" | "extasc" => password_struct.set_max(255),
+            "asc" | "ascii" => password_struct.set_max(126),
+            _ => {
+                eprintln!("Unknown encoding argument, setting to ASCII");
+                password_struct.set_max(126)
+            }
+        };
+
+        password_struct = password_struct.set_passwordtype(PasswordType::Regular);
+        return password_struct;
+    }
+    fn get_length(&self) -> u8 {
+        return self.length.to_owned();
+    }
+}
+
+impl CheckArgs for NewArgs {
+    fn check_arguments(self) -> Password {
+        //! Checks to see if the user has asked for spaces to be generated
+        //! in their password-- if so, then it returns
+
+        let mut password_struct = Password::new();
+
+        password_struct = match self.space {
+            true => password_struct.set_min(32),
+            false => password_struct.set_min(33),
+        };
+
+        password_struct = match self.encoding.as_str() {
+            "ext" | "extasc" => password_struct.set_max(255),
+            "asc" | "ascii" => password_struct.set_max(126),
+            _ => {
+                eprintln!("Unknown encoding argument, setting to ASCII");
+                password_struct.set_max(126)
+            }
+        };
+        password_struct = password_struct.set_passwordtype(PasswordType::Regular);
+        return password_struct;
+    }
+    fn get_length(&self) -> u8 {
+        return self.length.to_owned();
+    }
 }
