87b5353812c7459d79940818399b807f6c9d4bd3
withoutdishonor <westwardfishdme@gmail.com>
2026-03-31 22:30:36 -0500

    improved function docstring.

--- src/modules/steganographic/mime.rs
+++ src/modules/steganographic/mime.rs
@@ -1,12 +1,11 @@
 use std::{error::Error, fs, process::exit};
 
 pub fn check_magic(file: &String) -> Result<bool, Box<dyn Error>> {
-    // Checks the magic bytes of the inputted file to ensure that
-    // the image file is supported.
+    //! Checks the magic bytes of the inputted file to ensure that
+    //! the image file is supported. Supports `.jpg`, `.jpeg` and
+    //! `.png` types.
     let file = fs::read(file)?;
     let start = [file[0], file[1], file[2], file[3]];
-    // Supports .jpg, .jpeg, and .png
-
     match start {
         [0xff, 0xd8, _, _] => (),       //jpg, jpeg
         [0x89, 0x50, 0x4e, 0x47] => (), // PNG
