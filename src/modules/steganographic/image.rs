d8f4d7a3521c248b67711e885d0f0c1a0b11007c
withoutdishonor <westwardfishdme@gmail.com>
2026-04-15 19:41:10 -0500

    added a test module and implemented the skip feature

--- src/modules/steganographic/image.rs
+++ src/modules/steganographic/image.rs
@@ -66,16 +66,19 @@ impl OutputFormatting for String {
     }
 }
 
-fn create_password() -> Result<String, Box<dyn Error>> {
+fn create_password(skip_strength_test: bool) -> Result<String, Box<dyn Error>> {
+    //! Creates the user password as the AES-128 key for storing
+    //! the images in the password. Future additions may switch over
+    //! to using a hashed key instead of just the key itself.
     let mut password = String::new();
     eprint!("Enter your password (Must be 6-16 characters): ");
     let term = console::Term::stderr();
-    let mut low_strength_reask = true;
+    let mut low_strength_reask = !skip_strength_test;
+
     while password.len() < 6 || low_strength_reask {
         std::io::stderr().flush()?;
         password = term.read_secure_line()?;
         std::io::stderr().flush()?;
-
         // handle passkey length issues...
         match password.len() {
             0..6 => {
@@ -90,7 +93,6 @@ fn create_password() -> Result<String, Box<dyn Error>> {
                 continue;
             }
         }
-
         eprint!("Please re-enter your password: ");
         std::io::stderr().flush()?;
         let re_enter = term.read_secure_line()?;
@@ -103,9 +105,9 @@ fn create_password() -> Result<String, Box<dyn Error>> {
             continue;
         }
 
-        low_strength_reask = interactively_check_password(password.as_str());
-
+        // if the user skips the strength test; skip over the password strength check
         if low_strength_reask {
+            low_strength_reask = interactively_check_password(password.as_str());
             eprint!("\nEnter your password (Must be 6-16 characters): ");
         }
     }
@@ -118,13 +120,14 @@ pub fn store(
     output_fname: String,
     payload: &String,
     unencrypted: bool,
+    skip: bool,
 ) -> Result<(), Box<dyn Error>> {
     //! stores a payload into a given image. Takes the file and outp
 
     let mut string_to_store: Vec<u8> = Vec::from(payload.as_bytes().to_vec());
     if !unencrypted {
         // create a password
-        let key = create_password()?;
+        let key = create_password(skip)?;
         //end key
         string_to_store = string_to_store.encrypt(key.as_str())?;
     }
@@ -190,4 +193,34 @@ pub fn extract_raw_unencrypted(path: &String) -> Result<String, Box<dyn Error>> 
 
     let string = String::from_utf8(clean_buffer)?;
     Ok(string)
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[allow(unused)]
+    use super::*;
+    #[allow(unused)]
+    use crate::args::NewArgs;
+
+    #[test]
+    #[ignore = "this is implied behavior"]
+    fn test_skip_password() {
+        //! Although not explicitly tested, it is implied that ExistingArgs
+        //! would exhibit the exact same behavior that NewArgs does. since they
+        //! both use the uniform password logic.
+        let args = NewArgs {
+            encoding: String::from(""),
+            output: String::from(""),
+            name: String::from(""),
+            length: 1,
+            space: false,
+            unencrypted: false,
+            skip: true,
+        };
+        let password_check_would_not_skip = !args.skip;
+        if password_check_would_not_skip {
+            panic!("skip wouldn't skip the password");
+        }
+    }
 }
